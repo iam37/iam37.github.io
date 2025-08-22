41dcae5ff3a211dfe50ae31f3ed79c369dd499da
iam37 <135265890+iam37@users.noreply.github.com>
2025-08-22 20:17:09 +0000

    Deploying to gh-pages from @ iam37/iam37.github.io@dde0721967e3de7e53fce227ae867b6fb7dd8775 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -428,7 +428,12 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "projects-project-1",
+      },{id: "books-the-godfather",
+          title: 'The Godfather',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_godfather/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
